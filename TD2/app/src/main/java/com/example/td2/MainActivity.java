63ee5d6dfc2f5f79367d2a9f85e2e43a02c4c3d2
Francoise ZINSOU <zinsousabine7@gmail.com>
2021-11-17 08:49:05 +0100

    TD2

--- TD2/app/src/main/java/com/example/td2/MainActivity.java
+++ TD2/app/src/main/java/com/example/td2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.td2;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimePicker timePicker = (TimePicker) this.findViewById(R.id.time);
+        timePicker.setIs24HourView(true);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_etat);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.etatCiel, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
